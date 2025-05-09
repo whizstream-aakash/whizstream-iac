4329da7f51b64a445d2ff2094255226dfe9da3d1
Aakash1121 <aakashbhalani1121@gmail.com>
2025-05-08 23:49:05 -0400

    Changes in yaml file to update remote state s3  according to branch name

--- scripts/terraform_deploy_bootstrap.sh
+++ scripts/terraform_deploy_bootstrap.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 trap 'handle_error $LINENO' ERR
 trap 'handle_cancel' SIGINT SIGTERM
 
-# Accept workspace name as a parameter, default to "dev"
-WORKSPACE="${1:-dev}"
+# Accept branch name as a parameter, default to "dev"
+BRANCH="${1:-dev}"
 
 # Absolute paths
 MAIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/main"
 BOOTSTRAP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/bootstrap"
 
-# Debug: Print the workspace variable
-echo "Workspace: $WORKSPACE"
+# Debug: Print the current branch
+echo "Branch name: $BRANCH"
 
 # Check if the workspace is 'dev'
-if [ "$WORKSPACE" == "dev" ]; then  # Correct syntax with spaces around '=='
+if [ "$BRANCH" == "dev" ]; then  # Correct syntax with spaces around '=='
 
     # Get the absolute path to the directory containing this script
     echo "📦 Moving to bootstrap folder"
@@ -32,6 +32,6 @@ if [ "$WORKSPACE" == "dev" ]; then  # Correct syntax with spaces around '=='
     echo "Applying Terraform changes..."
     terraform apply -auto-approve
 else
-    echo "Not in 'dev' workspace. Skipping Terraform commands."
+    echo "Not in 'dev' branch. Skipping Terraform commands."
 fi  
 
